0b2aff7e4ead38ce1902f234523c2a584477c5f7
rmfadera10 <55060246+rmfadera10@users.noreply.github.com>
2019-11-02 10:57:58 +0545

    Registration page

--- app/src/main/java/com/letstravel/tourandtravelbookingsystem/RegisterActivity.java
+++ app/src/main/java/com/letstravel/tourandtravelbookingsystem/RegisterActivity.java
@@ -2,17 +2,13 @@ package com.letstravel.tourandtravelbookingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
-
 }
