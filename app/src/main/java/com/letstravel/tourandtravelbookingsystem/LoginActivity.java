fe228f7f6f10626eb28e2aa511244d7ea19ee32b
rmfadera10 <55060246+rmfadera10@users.noreply.github.com>
2019-11-06 21:52:26 +0545

    Login and Registration Design

--- app/src/main/java/com/letstravel/tourandtravelbookingsystem/LoginActivity.java
+++ app/src/main/java/com/letstravel/tourandtravelbookingsystem/LoginActivity.java
@@ -6,13 +6,25 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+private Button btnlogin, btnregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnlogin=(Button)findViewById(R.id.btnlogin);
+        btnregister=(Button)findViewById(R.id.btnregister);
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/letstravel/tourandtravelbookingsystem/RegisterActivity.java
+++ app/src/main/java/com/letstravel/tourandtravelbookingsystem/RegisterActivity.java
@@ -2,13 +2,27 @@ package com.letstravel.tourandtravelbookingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
-
+private Button btnlogin1, btnregister1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnlogin1=(Button)findViewById(R.id.btnlogin1);
+        btnregister1=(Button)findViewById(R.id.btnregister1);
+
+        btnlogin1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
